7aec3acf284d25416b55f208bbff10d8585ec198
guerrerocing <guerrerocing@outlook.com>
2023-11-20 00:15:44 -0600

    chore: Remove unused code

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { NextUIProvider } from "@nextui-org/react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <NextUIProvider>
+      <NextThemesProvider attribute="class" defaultTheme="dark">
+        <App />
+      </NextThemesProvider>
+    </NextUIProvider>
+  </React.StrictMode>
+);
